17aba37bfb8b68d7f554f60041adbd7c9a2a5a5e
Krishna <umesh.pradhan@reseapro.com>
2024-11-27 15:45:08 +0530

    pushing the code for form

--- src/app/component/Modal/OrderModal.tsx
+++ src/app/component/Modal/OrderModal.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import "./OrderModal.css";
+import { OrderDetails } from "@/app/utils/interfaces/types";
+import useFetchOrder from "@/app/hooks/authorDashboard/useFetchOrder";
 
-const OrderModal = ({modalHandler}: {modalHandler: () => void}) => {
+const OrderModal = ({ modalHandler }: { modalHandler: () => void }) => {
+    const userId: string | null = typeof window !== 'undefined' ? localStorage.getItem("user_id") : null
+    const { fetchOrder, error, loading }: { fetchOrder: OrderDetails[] | null, error: boolean, loading: boolean } = useFetchOrder(userId)
+    console.log({ fetchOrder });
 
-    {console.log(typeof(modalHandler))}
     return (
         <div className="modal-overlay">
             <div className="modal-container">
@@ -12,9 +16,32 @@ const OrderModal = ({modalHandler}: {modalHandler: () => void}) => {
                     <button onClick={modalHandler} className="close-button">×</button>
                 </div>
                 <div className="modal-body">
-                    <p><strong>User Name:</strong> asdfaf</p>
-                    <p><strong>Order ID:</strong> 134124</p>
-                    <p><strong>Service Category:</strong> sdfaf</p>
+                    {/* {fetchOrder?.map((val) => {
+
+                        return (
+                            <>
+                                <p><strong>User Name:</strong> sfas</p>
+                                <p><strong>Order ID:</strong> {val.order_id}</p>
+                                <p><strong>Service Category:</strong> </p>
+                                <p><strong>Service Type:</strong> sdfdsgs</p>
+                                <p><strong>Service Details:</strong> sdfgsdgs</p>
+                                <p><strong>Major Service Area:</strong> dsgsgs</p>
+                                <p><strong>Specific Service Area:</strong> sdfgsgs</p>
+                                <p><strong>Submit Date:</strong>314145</p>
+                                <p><strong>Delivery Date:</strong> 4352435</p>
+                                <p><strong>Journal Name:</strong> dgsgsg</p>
+                                <p><strong>Language:</strong>sdgweg</p>
+                                <p><strong>Word Count:</strong> 1353245</p>
+                                <p><strong>Instructions for Editor:</strong>afsfafa</p>
+                            </>
+
+                        )
+
+                    })} */}
+
+                    <p><strong>User Name:</strong> sfas</p>
+                    <p><strong>Order ID:</strong> 324324</p>
+                    <p><strong>Service Category:</strong> </p>
                     <p><strong>Service Type:</strong> sdfdsgs</p>
                     <p><strong>Service Details:</strong> sdfgsdgs</p>
                     <p><strong>Major Service Area:</strong> dsgsgs</p>
@@ -25,7 +52,10 @@ const OrderModal = ({modalHandler}: {modalHandler: () => void}) => {
                     <p><strong>Language:</strong>sdgweg</p>
                     <p><strong>Word Count:</strong> 1353245</p>
                     <p><strong>Instructions for Editor:</strong>afsfafa</p>
-                    
+
+
+
+
                 </div>
             </div>
         </div>
