f5f28ebfb38b92e859c9cf2e2b388666854a6fcb
Woruz <sabil.danish1997@gmail.com>
2024-12-02 11:53:59 +0530

    removed unneccesary imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,8 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
-import Sidebar from "./component/Sidebar/Sidebar";
-import Header from "./component/Header/Header";
 import "../../src/app/component/Header/Header.css";
-import { useEffect, useState } from "react";
-import { faFileAlt, faFileInvoice, faFileSignature } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Table from "./component/Table/Table";
-import { Dashboard } from "./component/Dashboard/Dashboard";
-import Coupon from "./component/Coupon/Coupon";
-import Quotation from "./component/Quotation/Quotation";
-import SubmitOrders from "./component/SubmitOrders/SubmitOrders";
-import Webinar from "./component/Webinar/Webinar";
-import SubmitManuscript from "./component/SubmitManuscript/SubmitManuscript";
 import { useRouter } from 'next/navigation'
-import TalkExpert from "./(OpenPages)/TalkExpert/page";
-import TalkToAnExpert from "./component/TalkExpert/TalkExpert";
-import ReferAColleague from "./component/ReferAColleague/ReferAColleague";
-import SampleRequest from "./component/SampleRquest/SampleRequest";
-import { MainContext } from "./context/MainContext";
 import { MainContextType } from "./utils/interfaces/types";
-import UpdateProfile from "./component/UpdateProfile/UpdateProfile";
-import PublicationForm from "./component/JournalPublicationForm/JournalPublicationForm";
+import { useState } from "react";
 
 export default function Home() {
   const [active, setActive] = useState<string>("Dashboard")
