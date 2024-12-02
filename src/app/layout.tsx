d9d0aeb2d744030ee32a80c936064a3e9324ca29
Woruz <sabil.danish1997@gmail.com>
2024-12-02 10:16:19 +0530

    reverting the changes back in layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client"
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -8,22 +7,22 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
 
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
-// export const metadata: Metadata = {
-//   title: "Author Dashboard",
-//   description: "Great like never before.",
-// };
+export const metadata: Metadata = {
+  title: "Author Dashboard",
+  description: "Great like never before.",
+};
 
 export default function RootLayout({
   children,
